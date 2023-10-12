4ff1e9d99813855f7aec70d08e789126bdd53b30
Marwen-Hammami <hammamimarwen.d@gmail.com>
2023-10-12 01:43:07 +0100

    HomeWork 4 - Fragments

--- app/src/main/java/tn/sim/gamerapp/HomeActivity.kt
+++ app/src/main/java/tn/sim/gamerapp/HomeActivity.kt
@@ -1,40 +1,91 @@
 package tn.sim.gamerapp
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
 import tn.sim.gamerapp.databinding.HomeBinding
 
 class HomeActivity: AppCompatActivity() {
 
     private lateinit var binding : HomeBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = HomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initBackArrowToolBar()
+        setUpToolBar()
+
+        setUpBottomNavigationBar()
+
 
     }
 
-    // Tool Bar Back Arrow START *************************************
-    fun initBackArrowToolBar() {
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.title = ""
-        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
 
-        //use back arrow from drawable
-        val upArrow = resources.getDrawable(R.drawable.ic_back_arrow)
-        supportActionBar?.setHomeAsUpIndicator(upArrow)
+    // Debut Methodes pour toolbar *********************************
+    fun setUpToolBar() {
+        val toolbar: Toolbar = binding.toolbar.topAppBar
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.getItemId()==android.R.id.home) {
-            finish()
+        val snack = Snackbar.make(binding.contextView,"Coming soon :)", Snackbar.LENGTH_LONG)
+            .setTextColor(ContextCompat.getColor(this, R.color.white))
+
+        when(item.itemId){
+            R.id.bell -> snack.show()
+            R.id.cart -> snack.show()
         }
+
         return super.onOptionsItemSelected(item)
     }
-    // Tool Bar Back Arrow END ***************************************
+    // Fin Methodes pour toolbar ***********************************
+
+
+    // Debut Methodes pour BottomBar *******************************
+    fun setUpBottomNavigationBar() {
+        minimumRequired()
+
+        changeTopBarTitleSwitchFragment()
+    }
+
+    fun minimumRequired() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
+    fun changeTopBarTitleSwitchFragment() {
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.newsFragment, R.id.storeFragment, R.id.profileFragment))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+    // Fin Methodes pour BottomBar *********************************
+
 
 }
+
+//enable cart icon
+
+//change toolbar title
---
+++ app/src/main/java/tn/sim/gamerapp/NewsFragment.kt
@@ -0,0 +1,39 @@
+package tn.sim.gamerapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.appbar.MaterialToolbar
+import tn.sim.gamerapp.databinding.FragmentNewsBinding
+
+
+class NewsFragment : Fragment() {
+
+    private lateinit var binding : FragmentNewsBinding
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        binding = FragmentNewsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar = requireActivity().findViewById<MaterialToolbar>(R.id.topAppBar)
+
+        enableShoppingCartIcon(toolbar)
+
+    }
+
+    fun enableShoppingCartIcon(toolbar: MaterialToolbar) {
+        val menu = toolbar.menu
+        val cartIem = menu.findItem(R.id.cart)
+
+        cartIem.isVisible = false
+    }
+}
